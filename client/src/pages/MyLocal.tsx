d6dd5120e9ceddb141547faf6ccf61fa612bbb1c
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-08-06 19:49:38 +0900

    Add a map

--- client/src/pages/MyLocal.tsx
+++ client/src/pages/MyLocal.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/common/Header/Header";
+import ReactMapGL from "react-map-gl";
 
 const MyLocal = () => {
+  const [viewport, setViewport] = useState({
+    width: "40vw",
+    height: "40vh",
+    latitude: 45.4211,
+    longitude: -75.6976,
+    zoom: 8,
+  });
   return (
     <div>
       <Header />
       <p>Refer Yelp or Airbnb</p>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={(nextViewport: any) => setViewport(nextViewport)}
+      >
+        markers here
+      </ReactMapGL>
     </div>
   );
 };
